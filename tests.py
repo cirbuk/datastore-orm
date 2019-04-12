fba73b8a35e359768d53df7c61bc71aa6ccea25c
chaitanya <chaitanyanettem@gmail.com>
2019-04-12 20:55:28 +0530

    more readable tests

--- tests.py
+++ tests.py
@@ -6,76 +6,81 @@ from datastore_orm import BaseModel
 from typing import List
 from dataclasses import dataclass
 from google.cloud import datastore
+from datetime import datetime
+from uuid import uuid4
 
 
 @dataclass
-class B(BaseModel):
-    x: int
-    y: int
-
-    @classmethod
-    def _sample(cls):
-        try:
-            return B._class_mapping
-        except:  # noqa: E722
-            cls._class_mapping = B(0, 1)
-            return cls._class_mapping
-
-
-@dataclass
-class C(BaseModel):
-    p: int
-    q: int
-
-    @classmethod
-    def _sample(cls):
-        try:
-            return C._class_mapping
-        except:  # noqa: E722
-            cls._class_mapping = C(0, 1)
-            return cls._class_mapping
-
-
-@dataclass
-class TestModel(BaseModel):
-    a: int
-    b: B
-    c: List[C]
+class Brand(BaseModel):
+    name: str
+    description: str
 
     @classmethod
     def _sample(cls):
         try:
             return cls._class_mapping
         except:  # noqa: E722
-            cls._class_mapping = TestModel(0, B._sample(), [C._sample()])
+            cls._class_mapping = cls("Sample Brand", "Hello from sample brand")
+            return cls._class_mapping
+
+    _exclude_from_indexes_ = ('description',)
+
+
+@dataclass
+class Price(BaseModel):
+    value: float
+    last_revised: datetime
+    currency: str
+
+    @classmethod
+    def _sample(cls):
+        try:
+            return cls._class_mapping
+        except:  # noqa: E722
+            cls._class_mapping = cls(9999.9, datetime.utcnow(), "USD")
             return cls._class_mapping
 
 
-def test_dotted_dict_to_object(dict_):
+@dataclass
+class Car(BaseModel):
+    uid: str
+    brand: Brand
+    prices: List[Price]
+
+    @classmethod
+    def _sample(cls):
+        try:
+            return cls._class_mapping
+        except:  # noqa: E722
+            cls._class_mapping = Car(uuid4(), Brand._sample(), [Price._sample()])
+            return cls._class_mapping
+
+
+def test_dotted_dict_to_object():
     """
     Trigger BaseModel._dotted_dict_to_object and test its functionality
     """
     BaseModel(None)
-    _DICT = {
-        "a": 1,
-        "b.x": 2,
-        "b.y": 3,
-        "c.p": [4, 5],
-        "c.q": [6, 7]
+    dict_ = {
+        "uid": str(uuid4()),
+        "brand.name": "Mercedes",
+        "brand.description": "Generic luxury car",
+        "prices.value": [9888, 6785],
+        "prices.last_revised": [datetime.utcnow(), datetime.utcnow()],
+        "prices.currency": ["USD", "EUR"]
     }
-    obj = test_dotted_dict_to_object(_DICT)
-    print(obj)
-    return TestModel._dotted_dict_to_object(dict_)
+    return Car._dotted_dict_to_object(dict_)
 
 
 def test_put():
-    BaseModel(datastore.Client())
-    test_model = TestModel(0, B(1, 2), [C(3, 4), C(5, 6)])
-    test_model.put()
+    BaseModel(datastore.Client(namespace="Beta"))
+    car = Car(str(uuid4()), Brand("Mercedes", "Generic Brand"),
+              [Price(9888, datetime.utcnow(), "USD"), Price(6899, datetime.utcnow(), "GBP")])
+    car.put()
 
 
 def test_query():
-    query = TestModel.query()
+    query = Car.query()
     query_iter = query.fetch(start_cursor=None, limit=1)
     for page in query_iter.pages:
         print(list(page))
